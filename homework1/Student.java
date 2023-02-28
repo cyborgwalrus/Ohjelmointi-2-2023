ad7dfe9dd8bae2e9906bd75d0424656bb3a77128
Matias Björklund <matias_bjorklund@hotmail.com>
2023-02-28 16:45:38 +0200

    moved Student.java into homework1 folder

--- homework1/Student.java
+++ homework1/Student.java
@@ -1,28 +1,10 @@
+package homework1;
+import static homework1.ConstantValues.*;
+
 import java.lang.Math;
 import java.time.LocalDate;
 
-class ConstantValues {
-    public static final String NO_NAME = "No name";
-    public static final String NO_TITLE = "No title";
-    public static final String NO_BIRTHDATE = "Not available";
-    public static final String INVALID_BIRTHDAY = "Invalid birthday!";
-    public static final String INCORRECT_CHECKMARK = "Incorrect check mark!";
-
-    public static final int MIN_ID = 1;
-    public static final int MAX_ID = 100;
-
-    public static final double MIN_CREDIT = 0.0;
-    public static final double MAX_CREDITS = 300.0;
-
-    public static final double BACHELOR_CREDITS = 180.0;
-    public static final double MASTER_CREDITS = 120.0;
-
-    public static final int CURRENT_YEAR = LocalDate.now().getYear();
-    public static final String INDENTATION = " ".repeat(8);
-
-}
-
-public class Student extends ConstantValues {
+public class Student{
     // Attributes
     private String firstName = NO_NAME;
     private String lastName = NO_NAME;
