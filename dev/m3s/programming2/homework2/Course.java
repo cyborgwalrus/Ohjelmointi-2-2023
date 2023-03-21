9df58bcbe2c850be6912221b43e75144d3cac116
Matias Björklund <32132417+cyborgwalrus@users.noreply.github.com>
2023-03-21 18:41:10 +0200

    Created Course methods (minus toString)

--- dev/m3s/programming2/homework2/Course.java
+++ dev/m3s/programming2/homework2/Course.java
@@ -22,7 +22,78 @@ public class Course {
 
     public Course(Course course) {
     }
-    // TODO getName, setName, getCourseTypeString, getCourseType,
-    // setCourseType, getCourseCode, setCourseCode, getCourseBase, getPeriod,
-    // setPeriod, getCredits, setCredits, isNumericGrade, setNumericGrade, toString
+
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (name != null && !name.equals(""))
+            this.name = name;
+    }
+
+    public String getCourseTypeString() {
+        switch (courseType) {
+            case 0:
+                return "Optional";
+            case 1:
+                return "Mandatory";
+            default:
+                return "";
+        }
+    }
+
+    public int getCourseType() {
+        return courseType;
+    }
+
+    public void setCourseType(final int type) {
+        if (type != 1 || type != 0)
+            ;
+        this.courseType = type;
+    }
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(final int courseCode, Character courseBase) {
+        boolean courseCodeIsValid = 0 < courseCode && courseCode < 1000000;
+        boolean courseBaseIsValid = courseBase == 'A' || courseBase == 'P' || courseBase == 'S';
+
+        if (courseCodeIsValid && courseBaseIsValid) {
+            this.courseCode = Integer.toString(courseCode) + courseBase;
+        }
+    }
+
+    public Character getCourseBase() {
+        return courseBase;
+    }
+
+    public int getPeriod() {
+        return period;
+    }
+
+    public void setPeriod(final int period) {
+        if (MIN_PERIOD <= period && period <= MAX_PERIOD)
+            this.period = period;
+    }
+
+    public double getCredits(){
+        return credits;
+    }
+
+    private void setCredits(final double credits){
+        if(0 <= credits && credits <= MAX_COURSE_CREDITS)
+            this.credits = credits;
+    }
+
+    public boolean isNumericGrade(){
+        return numericGrade;
+    }
+    public void setNumericGrade(boolean numericGrade){
+        this.numericGrade = numericGrade;
+    }
+    // TODO toString
 }
