2c5f2131bfb0ab656632a46ac3da992262fe5877
Matias Björklund <matias_bjorklund@hotmail.com>
2023-04-29 19:51:22 +0300

    Implemented MonthlyPayment and HoursBasedPayment

---
+++ dev/m3s/programming2/homework3/HourBasedPayment.java
@@ -0,0 +1,29 @@
+package dev.m3s.programming2.homework3;
+
+public class HourBasedPayment implements Payment{
+    private double eurosPerHour;
+    private double hours;
+
+    public double getEurosPerHour() {
+        return this.eurosPerHour;
+    }
+
+    public void setEurosPerHour(double eurosPerHour) {
+        if(eurosPerHour > 0.0)
+            this.eurosPerHour = eurosPerHour;
+    }
+
+    public double getHours() {
+        return this.hours;
+    }
+
+    public void setHours(double hours) {
+        if(hours > 0.0)
+            this.hours = hours;
+    }
+    
+    public double calculatePayment(){
+        return getHours() * getEurosPerHour();
+    }
+    
+}
