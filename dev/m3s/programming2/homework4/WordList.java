a8425b46226cf36eab3fba9535b18ffbb7bf8913
Matias Björklund <matias_bjorklund@hotmail.com>
2023-05-07 18:04:46 +0300

    Implemented theWordsWithCharacters

--- dev/m3s/programming2/homework4/WordList.java
+++ dev/m3s/programming2/homework4/WordList.java
@@ -33,6 +33,27 @@ public class WordList {
         return newWordList;
     }
 
+    public WordList theWordsWithCharacters(String inputString) {
+        int lettersInInputString;
+        lettersInInputString = inputString.replace("_", "").length();
+
+        WordList wordListWithSameLength = this.theWordsOfLength(inputString.length());
+        WordList outputWordList = new WordList();
+        int MatchingLettersInWord;
+        for (String word : wordListWithSameLength.giveWords()) {
+            MatchingLettersInWord = 0;
+
+            for (int i = 0; i < inputString.length(); i++) {
+                if (inputString.charAt(i) == word.charAt(i))
+                    MatchingLettersInWord++;
+            }
+
+            if (MatchingLettersInWord == lettersInInputString)
+                outputWordList.add(word);
+        }
+        return outputWordList;
+    }
+
     // Optional methods
     public String getRandomWord() {
         return wordList.get(rand.nextInt(wordList.size()));
@@ -52,19 +73,35 @@ public class WordList {
         }
     }
 
+    public void remove(String word) {
+        wordList.remove(word);
+    }
+
     public static void main(String[] args) {
+        // Testing wordList creation
         WordList wordList = null;
+        System.out.println("Creating wordlist:");
+
         try {
             wordList = new WordList("words.txt");
             System.out.println(String.format("Words loaded: %d", wordList.size()));
-            System.out.println(String.format("Random word: %s", wordList.getRandomWord()));
+            System.out.println(String.format("Random word: %s\n", wordList.getRandomWord()));
         } catch (IOException e) {
             System.out.println("File couldn't be opened.");
         }
+
+        // Testing theWordsOfLength
         int wordLength = 5;
         System.out.println(String.format("Creating new wordlist based on word length: %d", wordLength));
         WordList newWordList = wordList.theWordsOfLength(wordLength);
         System.out.println(String.format("Words loaded: %d", newWordList.size()));
-        System.out.println(String.format("Random word: %s", newWordList.getRandomWord()));
+        System.out.println(String.format("Random word: %s\n", newWordList.getRandomWord()));
+
+        // Testing TheWordsWithCharacters
+        String letters = "_a_e_";
+        System.out.println(String.format("Creating new wordlist based on letters : %s", letters));
+        newWordList = wordList.theWordsWithCharacters(letters);
+        System.out.println(String.format("Words loaded: %d", newWordList.size()));
+        System.out.println(String.format("Random word: %s\n", newWordList.getRandomWord()));
     }
 }
