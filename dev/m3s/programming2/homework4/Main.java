9eee0b2c5bace25c7ab179510a9afe6985782fa6
Matias Björklund <matias_bjorklund@hotmail.com>
2023-05-07 20:28:56 +0300

    Added error handling to input

--- dev/m3s/programming2/homework4/Main.java
+++ dev/m3s/programming2/homework4/Main.java
@@ -1,11 +1,12 @@
 package dev.m3s.programming2.homework4;
+
 import java.io.*;
 
 public class Main {
     static int GUESSES = 10;
 
     public static void main(String[] args) {
-        //Setup wordList
+        // Setup wordList
         WordList wordList = null;
         try {
             wordList = new WordList("words.txt");
@@ -14,24 +15,23 @@ public class Main {
             System.exit(1);
         }
 
-        //Setup Hangman
+        // Setup Hangman
         Hangman hangMan = new Hangman(wordList, GUESSES);
         String hiddenWord = hangMan.word();
 
-        //Main loop
+        // Main loop
         String outputString;
         while (hangMan.theEnd() == false && hangMan.guessesLeft() > 0) {
             outputString = "";
             outputString += "The hidden word...\n\n";
-            
-            //Build hidden word string
+
+            // Build hidden word string
             String hiddenWordString = "";
-            for(int i=0; i < hiddenWord.length(); i++){
+            for (int i = 0; i < hiddenWord.length(); i++) {
                 char c = hiddenWord.charAt(i);
-                if(hangMan.guesses().contains(c)){
+                if (hangMan.guesses().contains(c)) {
                     hiddenWordString += c + " ";
-                }
-                else{
+                } else {
                     hiddenWordString += "* ";
                 }
             }
@@ -39,25 +39,31 @@ public class Main {
 
             outputString += String.format("Guesses left: %d\n", hangMan.guessesLeft());
             outputString += String.format("Guessed letters: %s\n\n", hangMan.guesses().toString());
-            //outputString complete, printing...
+            // outputString complete, printing...
             System.out.print(outputString);
 
-            //Handling user input
-            char inputChar = System.console().readLine("Guess a letter: ").charAt(0);
-            if(Character.isLetter(inputChar))
+            // Handling user input
+            char inputChar = ' ';
+            try {
+
+                System.out.print("Guess a letter: ");
+                inputChar = System.console().readLine().charAt(0);
+            } catch (Exception e) {
+                System.out.print("Something went wrong.\n");
+            }
+            if (Character.isLetter(inputChar))
                 hangMan.guess(inputChar);
-            
+
         }
-        //Game over
+        // Game over
         outputString = "\n";
-        if(hangMan.guessesLeft() > 0)
+        if (hangMan.guessesLeft() > 0)
             outputString += "Congratulations! You won!!!\n";
         else
             outputString += "Sorry, you lost!\n";
-        
+
         outputString += String.format("The hidden word was: \"%s\"\n", hangMan.word());
         System.out.println(outputString);
 
-        
     }
 }
