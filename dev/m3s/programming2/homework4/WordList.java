85ff8b465f07a521a101d8fd445d5feb94b138b2
Matias Björklund <matias_bjorklund@hotmail.com>
2023-05-07 15:10:53 +0300

    Implemented getRandomWord

--- dev/m3s/programming2/homework4/WordList.java
+++ dev/m3s/programming2/homework4/WordList.java
@@ -3,6 +3,7 @@ import java.util.*;
 
 public class WordList {
     private List<String> wordList = new ArrayList<String>();
+    private Random rand = new Random();
 
     public WordList(String WordsFile) throws IOException {
 
@@ -20,6 +21,10 @@ public class WordList {
         return wordList;
     }
 
+    public String getRandomWord(){
+        return wordList.get(rand.nextInt(wordList.size()));
+    }
+
     public int size() {
         return wordList.size();
     }
@@ -28,6 +33,7 @@ public class WordList {
         try {
             WordList wordList = new WordList("words.txt");
             System.out.println(String.format("Words loaded: %d", wordList.size()));
+            System.out.println(String.format("Random word: %s", wordList.getRandomWord()));
         } catch (IOException e) {
             System.out.println("File couldn't be opened.");
         }
